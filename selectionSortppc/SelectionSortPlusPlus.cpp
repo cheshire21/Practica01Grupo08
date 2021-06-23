86208ce1f907572a85485031049e506dde33948e
cheshire <coren114@gmail.com>
2021-06-23 16:08:31 -0500

    cambios y resultados selectionSort

--- selectionSortppc/SelectionSortPlusPlus.cpp
+++ selectionSortppc/SelectionSortPlusPlus.cpp
@@ -16,62 +16,6 @@ using namespace std;
 using namespace std::chrono;
 
 float t0, t1;
-void inserta_en_monticulo(int arr[], int n)
-{
-	int k, aux;
-	bool band;
-	for (int i = 2; i <= n; i++) {
-		k = i;
-		band = true;
-		while ((k > 1) && (band == true)) {
-			band = false;
-			aux = arr[k / 2];
-			arr[k / 2] = arr[k];
-			arr[k] = aux;
-			k = k / 2;
-			band = true;
-		}
-	}
-}
-
-void elimina_monticulo(int arr[], int n)
-{
-	int aux, izq, der, k, ap;
-	bool band;
-	for (int i = n; i >= 2; i--) {
-		aux = arr[i];
-		arr[i] = arr[1];
-		izq = 2;
-		der = 3;
-		k = 1;
-		band = true;
-		int mayor;
-		while ((izq < i) && (band == true)) {
-			mayor = arr[izq];
-			ap = izq;
-			if ((mayor < arr[der]) && (der != i)) {
-				mayor = arr[der];
-				ap = der;
-			}
-			if (aux < mayor) {
-				arr[k] = arr[ap];
-				k = ap;
-			}
-			else {
-				band = false;
-			}
-			izq = k * 2;
-			der = k * 2 + 1;
-		}
-		arr[k] = aux;
-	}
-}
-//Funcion heap sort
-void heapSort(int arr[], int n) {
-	inserta_en_monticulo(arr, n);
-	elimina_monticulo(arr, n);
-}
-
 
 void swap(long int* a, long int* b)
 {
@@ -80,7 +24,7 @@ void swap(long int* a, long int* b)
 	*b = tmp;
 }
 
-void selectionSort(long int arr[], long int n)
+void selectionSort( int arr[],  int n)
 {
 	long int i, j, midx;
 
@@ -95,11 +39,17 @@ void selectionSort(long int arr[], long int n)
 
 		// Swap the found minimum element
 		// with the first element
-		swap(&arr[midx], &arr[i]);
+		swap(arr[midx], arr[i]);
 	}
+	
 }
 
-
+string getCurrentDir() {
+    char buff[MAX_PATH];
+    GetModuleFileName( NULL, buff, MAX_PATH );
+    string::size_type position = string( buff ).find_last_of( "\\/" );
+    return string( buff ).substr( 0, position);
+}
 int main()
 {
 	int arr2[] = { 100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000 };
@@ -112,7 +62,7 @@ int main()
 		int* arr = nullptr;
 		arr = new int[num];
 		//ifstream file("E:\\2021\\UNSA\\Semestre I\\Algoritmos\\Practica\\Trabajo en equipo\\Practica01Grupo08\\Luis\\data\\archivo" + to_string(num) + ".txt");
-		ifstream file("data\\archivo" + to_string(num) + ".txt");
+		ifstream file(getCurrentDir() +"\\data\\archivo" + to_string(num) + ".txt");
 		if (file.is_open())
 		{
 			for (int i = 0; i < num; ++i)
@@ -125,7 +75,7 @@ int main()
 
 		auto t0 = high_resolution_clock::now();
 
-		heapSort(arr, num);
+		selectionSort(arr, num);
 
 		auto t1 = high_resolution_clock::now();
 
@@ -137,7 +87,7 @@ int main()
 
 		str1 = str1 + to_string(num) + "," + to_string(ms_double.count()) + "\n";
 	}
-	ofstream MyFile("SelectionSortC.txt");
+	ofstream MyFile(getCurrentDir() +"\\SelectionSortC.txt");
 	MyFile << str1 << endl;
 	MyFile.close();
 	return 0;
--- selectionSortppc/selectionSort.py
+++ selectionSortppc/selectionSort.py
@@ -25,21 +25,21 @@ def selection_sort(L):
 
 
 str1 = ''
-#arr2 = [100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
-arr2 = [100,200]
+arr2 = [100,200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
+# arr2 = [100,200]
 canti_data=100000
 for canti_data in arr2:
 	print(canti_data)
-	arr = np.loadtxt(f"D:/trabajos/Maestria/EDM/trabajo1/SelecionSort/data/archivo{canti_data}.txt", dtype=int)
+	arr = np.loadtxt(f"./data/archivo{canti_data}.txt", dtype=int)
 	inicio = time.time()
 	selection_sort(arr)
 	fin = time.time()
 	result = f'{(fin - inicio)*1000}'
 	if canti_data <= 100000:
-		str1+= str(canti_data) + ',' +  str(result) + '\n'
+		str1+= str(canti_data) + ' ' +  str(result) + '\n'
 
-print("Inicio de creación del archivo")
-file = open("./SelectionSortP.csv", "w")
+
+file = open("./SelectionSortPy.txt", "w")
 file. write(str1)    
 file. close()
 
